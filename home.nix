88e8f00d70855cde6579017d909d4f7707709576
Louis Pearson <opensource@louispearson.work>
2022-07-11 23:21:29 -0600

    Added final pdf dependency

--- home.nix
+++ home.nix
@@ -4,7 +4,7 @@ let
   tex = (pkgs.texlive.combine {
     inherit (pkgs.texlive) scheme-basic
       dvisvgm dvipng # for preview and export as html
-      wrapfig amsmath ulem hyperref capt-of;
+      wrapfig amsmath ulem hyperref capt-of metafont;
   });
 in
 {
