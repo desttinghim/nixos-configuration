770fe5aae965db957c26d6ea2b8242c66f5cee3b
Louis Pearson <opensource@louispearson.work>
2023-02-20 18:57:15 -0700

    Set plasma wayland as default session

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -131,9 +131,13 @@
       '';
   };
 
-  services.xserver.enable = true;
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
+  services.xserver = {
+    enable = true;
+    desktopManager.plasma5.enable = true;
+    displayManager.sddm = {
+      enable = true;
+    };
+  };
 
   services.logind = {
     lidSwitch = "suspend-then-hibernate";
@@ -174,16 +178,16 @@
     ];
   };
 
-  services.tlp = {
-    enable = true;
-    settings = {
-      CPU_BOOST_ON_BAT = 0;
-      CPU_SCALING_GOVERNOR_ON_BATTERY = "powersave";
-      START_CHARGE_THRESH_BAT0 = 90;
-      STOP_CHARGE_THRESH_BAT0 = 97;
-      RUNTIME_PM_ON_BAT = "auto";
-    };
-  };
+  # services.tlp = {
+  #   enable = true;
+  #   settings = {
+  #     CPU_BOOST_ON_BAT = 0;
+  #     CPU_SCALING_GOVERNOR_ON_BATTERY = "powersave";
+  #     START_CHARGE_THRESH_BAT0 = 90;
+  #     STOP_CHARGE_THRESH_BAT0 = 97;
+  #     RUNTIME_PM_ON_BAT = "auto";
+  #   };
+  # };
 
   systemd.sleep.extraConfig = "HibernateDelaySec=1h";
 
