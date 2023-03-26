49b89cc1bf3f98393cdab220cff13e455374c658
Louis Pearson <opensource@louispearson.work>
2023-03-25 20:12:55 -0600

    32-bit opengl binaries (for GameMaker on wine)

--- nixos/framework/hardware-configuration.nix
+++ nixos/framework/hardware-configuration.nix
@@ -45,6 +45,7 @@
   };
   hardware.opengl = {
     enable = true;
+    driSupport32Bit = true;
     extraPackages = with pkgs; [
       intel-media-driver  # LIBVA_DRIVER_NAME=iHD
       vaapiIntel          # LIBVA_DRIVER_NAME=i965
