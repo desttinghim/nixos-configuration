364508d4fa7ecadbf799b693ee633d9cd7e826c9
Louis Pearson <opensource@louispearson.work>
2024-03-20 16:57:59 -0600

    Run flake update, bump zig version to latest mach nominated

--- home.nix
+++ home.nix
@@ -87,7 +87,7 @@ in
     cargo
     rustfmt
     clang
-    inputs.zig.packages.${system}.master-2024-01-07
+    inputs.zig.packages.${system}.master-2024-03-08
     inputs.zls.packages.${system}.default
     nodePackages.prettier
     nodejs
